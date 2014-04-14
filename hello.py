c9db190d0b063d8ef872d02e1e6ede271b0f209f
howead <howead@colorado.edu>
2014-04-13 21:21:00 -0600

    added google api's

--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
 from flask import Flask, session, redirect, url_for, escape, request
 from flask import render_template
+import sqlalchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80),unique=False)
+
+    def __init__(self, email, password):
+        self.username = email
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route("/", methods=['GET', 'POST'])
 def welcome():
